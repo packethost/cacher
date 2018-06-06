c445e427981373873cdbe8d5bb3e596dff8bec5e
Manuel Mendez <mmendez534@gmail.com>
2018-06-06 14:37:54 -0400

    db: add deleted_at column

--- db/docker-entrypoint-initdb.d/cacher-init.sql
+++ db/docker-entrypoint-initdb.d/cacher-init.sql
@@ -1,8 +1,10 @@
 CREATE TABLE hardware (
 	id UUID UNIQUE
 	, inserted_at TIMESTAMPTZ
+	, deleted_at TIMESTAMPTZ
 	, data JSONB
 );
 
 CREATE INDEX idx_id ON hardware (id);
+CREATE INDEX idx_deleted_at ON hardware (deleted_at NULLS FIRST);
 CREATE INDEX idxgin_type ON hardware USING GIN (data JSONB_PATH_OPS);
