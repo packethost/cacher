5c0eea8b018b21aee0332682240afe4e071ec360
Manuel Mendez <mmendez534@gmail.com>
2018-06-25 16:58:18 -0400

    tls: avoid subshell

--- tls/gencerts.sh
+++ tls/gencerts.sh
@@ -2,13 +2,13 @@
 
 set -o errexit -o nounset -o pipefail
 
-if ! ([[ -r ca.json ]] && [[ -r ca.pem ]] && [[ -r ca-key.pem ]]); then
+if ! { [[ -r ca.json ]] && [[ -r ca.pem ]] && [[ -r ca-key.pem ]]; }; then
 	sed "s|@FACILITY@|$FACILITY|g" </tls/ca.in.json >ca.json
 	cfssl gencert \
 		-initca ca.json | cfssljson -bare ca
 	rm -f server-csr.json server-*.pem
 fi
-if ! ([[ -r server-csr.json ]] && [[ -r server.pem ]] && [[ -r server-key.pem ]]); then
+if ! { [[ -r server-csr.json ]] && [[ -r server.pem ]] && [[ -r server-key.pem ]]; }; then
 	sed "s|@FACILITY@|$FACILITY|g" </tls/server-csr.in.json >server-csr.json
 	cfssl gencert \
 		-ca=ca.pem \
