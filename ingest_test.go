219ca037e03a1d45ba2fb9ed52514f5795661ec2
Manuel Mendez <mmendez534@gmail.com>
2019-04-16 19:30:57 -0400

    ingest: add test for fetchFacility

---
+++ ingest_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"net/url"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/packethost/packngo"
+	"github.com/packethost/pkg/log"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestMain(m *testing.M) {
+	os.Setenv("PACKET_ENV", "test")
+	os.Setenv("PACKET_VERSION", "0")
+	os.Setenv("ROLLBAR_DISABLE", "1")
+	os.Setenv("ROLLBAR_TOKEN", "1")
+
+	logger, _, _ = log.Init("github.com/packethost/cacher")
+	setupMetrics("testing")
+
+	os.Exit(m.Run())
+}
+
+func TestFetchFacility(t *testing.T) {
+	defer gock.Off()
+	facility := "testing" + strconv.Itoa(rand.Int())
+
+	pages := []map[string]interface{}{
+		{
+			"meta": map[string]interface{}{
+				"current_page": 1,
+				"last_page":    2,
+				"total":        2,
+			},
+			"Hardware": []map[string]interface{}{
+				{
+					"id": "1",
+				},
+			},
+		},
+		{
+			"meta": map[string]interface{}{
+				"current_page": 2,
+				"last_page":    2,
+				"total":        2,
+			},
+			"Hardware": []map[string]interface{}{
+				{
+					"id": "2",
+				},
+			},
+		},
+	}
+	for i, m := range pages {
+		gock.New("https://api.packet.net").
+			Get("staff/cacher/hardware").
+			MatchParam("facility", facility).
+			MatchParam("page", strconv.Itoa(i+1)).
+			Reply(200).
+			JSON(m)
+	}
+
+	ch := make(chan []map[string]interface{}, len(pages)*10)
+	u, err := url.Parse("https://api.packet.net")
+	assert := require.New(t)
+	assert.NoError(err)
+
+	client := packngo.NewClientWithAuth(os.Getenv("PACKET_CONSUMER_TOKEN"), os.Getenv("PACKET_API_AUTH_TOKEN"), nil)
+	err = fetchFacility(context.TODO(), client, u, facility, ch)
+	assert.NoError(err)
+	assert.Len(ch, len(pages))
+	assert.True(gock.IsDone())
+
+	for _, m := range pages {
+		p := <-ch
+		assert.Equal(m["Hardware"], p)
+	}
+}
