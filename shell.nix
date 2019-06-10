e123605b34c3df69aafbd21b019c08cc671d423d
Manuel Mendez <mmendez534@gmail.com>
2019-06-10 14:24:18 -0400

    nix: change to mkShell and run through nixfmt

--- shell.nix
+++ shell.nix
@@ -1,22 +1,11 @@
-let
-  _pkgs = import <nixpkgs> {};
-in
-{ pkgs ? import (_pkgs.fetchFromGitHub { owner = "NixOS";
-                                         repo = "nixpkgs";
-                                         rev = "18.09";
-                                         sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
-                                       }) {}
-}:
+let _pkgs = import <nixpkgs> { };
+in { pkgs ? import (_pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "18.09";
+    sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
+  }) { } }:
 
 with pkgs;
 
-stdenv.mkDerivation rec {
-  name = "cacher";
-  env = buildEnv { name = name; paths = buildInputs; };
-  buildInputs = [
-    cfssl
-    dep
-    go
-    protobuf
-  ];
-}
+mkShell { buildInputs = [ cfssl dep go protobuf ]; }
