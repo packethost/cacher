2e57c2a0c9ef746587b62df5e524b5eb75c68270
Mike Mason <mason@packet.com>
2020-07-01 17:18:49 -0500

    add insecure support to grpc client
    when using a loadbalancer that's handling the tls termination for
    cacher, cacher server doesn't handle the certificates itself. Due
    to this, when attempting to have services which implement cacher's
    client library, and happen to live within the same container
    orchestrator, instead of having to go through the ingres, to handle
    the tls termination, it would be preferred to talk directly to the
    cacher container.
    
    This adds support to allow the cacher client library to not attempt
    to connect to cacher with the discovered certs.

--- client/main.go
+++ client/main.go
@@ -15,6 +15,14 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
+func isTrue(s string) bool {
+	s = strings.ToLower(s)
+	if s == "1" || s == "t" || s == "true" {
+		return true
+	}
+	return false
+}
+
 func New(facility string) (cacher.CacherClient, error) {
 	lookupAuthority := func(service, facility string) (string, error) {
 		_, addrs, err := net.LookupSRV(service, "tcp", "cacher."+facility+".packet.net")
@@ -29,32 +37,43 @@ func New(facility string) (cacher.CacherClient, error) {
 		return fmt.Sprintf("%s:%d", strings.TrimSuffix(addrs[0].Target, "."), addrs[0].Port), nil
 	}
 
-	certURL := os.Getenv("CACHER_CERT_URL")
-	if certURL == "" {
-		auth, err := lookupAuthority("http", facility)
-		if err != nil {
-			return nil, err
+	var securityOption grpc.DialOption
+
+	insecure := os.Getenv("CACHER_INSECURE")
+
+	if isTrue(insecure) {
+		securityOption = grpc.WithInsecure()
+	} else {
+		certURL := os.Getenv("CACHER_CERT_URL")
+		if certURL == "" {
+			auth, err := lookupAuthority("http", facility)
+			if err != nil {
+				return nil, err
+			}
+			certURL = "https://" + auth + "/cert"
 		}
-		certURL = "https://" + auth + "/cert"
-	}
-	resp, err := http.Get(certURL)
-	if err != nil {
-		return nil, errors.Wrap(err, "fetch cert")
-	}
-	defer resp.Body.Close()
+		resp, err := http.Get(certURL)
+		if err != nil {
+			return nil, errors.Wrap(err, "fetch cert")
+		}
+		defer resp.Body.Close()
 
-	certs, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errors.Wrap(err, "read cert")
+		certs, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, errors.Wrap(err, "read cert")
+		}
+
+		cp := x509.NewCertPool()
+		ok := cp.AppendCertsFromPEM(certs)
+		if !ok {
+			return nil, errors.New("parsing cert")
+		}
+		creds := credentials.NewClientTLSFromCert(cp, "")
+
+		securityOption = grpc.WithTransportCredentials(creds)
 	}
 
-	cp := x509.NewCertPool()
-	ok := cp.AppendCertsFromPEM(certs)
-	if !ok {
-		return nil, errors.New("parsing cert")
-	}
-	creds := credentials.NewClientTLSFromCert(cp, "")
-
+	var err error
 	grpcAuthority := os.Getenv("CACHER_GRPC_AUTHORITY")
 	if grpcAuthority == "" {
 		grpcAuthority, err = lookupAuthority("grpc", facility)
@@ -62,7 +81,8 @@ func New(facility string) (cacher.CacherClient, error) {
 			return nil, err
 		}
 	}
-	conn, err := grpc.Dial(grpcAuthority, grpc.WithTransportCredentials(creds))
+
+	conn, err := grpc.Dial(grpcAuthority, securityOption)
 	if err != nil {
 		return nil, errors.Wrap(err, "connect to cacher")
 	}
