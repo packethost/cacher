cc038c3738004e857876201f192df7519f77e752
Mike Mason <mason@packet.com>
2020-08-25 08:40:54 -0500

    update environment variables to use packethost/pkg/env
    Signed-off-by: Mike Mason <mason@packet.com>

--- client/main.go
+++ client/main.go
@@ -6,10 +6,10 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/packethost/cacher/protos/cacher"
+	"github.com/packethost/pkg/env"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -29,7 +29,7 @@ func New(facility string) (cacher.CacherClient, error) {
 		return fmt.Sprintf("%s:%d", strings.TrimSuffix(addrs[0].Target, "."), addrs[0].Port), nil
 	}
 
-	certURL := os.Getenv("CACHER_CERT_URL")
+	certURL := env.Get("CACHER_CERT_URL")
 	if certURL == "" {
 		auth, err := lookupAuthority("http", facility)
 		if err != nil {
@@ -55,7 +55,7 @@ func New(facility string) (cacher.CacherClient, error) {
 	}
 	creds := credentials.NewClientTLSFromCert(cp, "")
 
-	grpcAuthority := os.Getenv("CACHER_GRPC_AUTHORITY")
+	grpcAuthority := env.Get("CACHER_GRPC_AUTHORITY")
 	if grpcAuthority == "" {
 		grpcAuthority, err = lookupAuthority("grpc", facility)
 		if err != nil {
