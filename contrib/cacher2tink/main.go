3f8d1ccbd99ad22eca6bcfd96b891876338f8343
Manuel Mendez <mmendez@equinix.com>
2020-11-13 18:57:56 -0500

    cacher2tink: parse all of stdin
    No reason to limit cacher2tink to only one json object per run if
    there's more in stdin.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- contrib/cacher2tink/main.go
+++ contrib/cacher2tink/main.go
@@ -27,102 +27,105 @@ func main() {
 		os.Exit(1)
 	}
 
-	var m map[string]interface{}
-	err := json.NewDecoder(os.Stdin).Decode(&m)
-	if err != nil {
-		panic(err)
-	}
-	if m["instance"] == nil {
-		m["instance"] = map[string]interface{}{}
-	}
-	instance := m["instance"].(map[string]interface{})
+	dec := json.NewDecoder(os.Stdin)
+	for {
+		var m map[string]interface{}
+		err := dec.Decode(&m)
+		if err != nil {
+			panic(err)
+		}
+		if m["instance"] == nil {
+			m["instance"] = map[string]interface{}{}
+		}
+		instance := m["instance"].(map[string]interface{})
 
-	buf := bytes.NewBuffer(nil)
-	err = json.NewEncoder(buf).Encode(m)
-	if err != nil {
-		panic(err)
-	}
+		buf := bytes.NewBuffer(nil)
+		err = json.NewEncoder(buf).Encode(m)
+		if err != nil {
+			panic(err)
+		}
 
-	c := packet.DiscoveryCacher{}
-	err = json.NewDecoder(buf).Decode(&c)
-	if err != nil {
-		panic(err)
-	}
+		c := packet.DiscoveryCacher{}
+		err = json.NewDecoder(buf).Decode(&c)
+		if err != nil {
+			panic(err)
+		}
 
-	d := packet.HardwareTinkerbellV1{
-		ID: c.ID,
-		Network: packet.Network{
-			Interfaces: func() []packet.NetworkInterface {
-				ifaces := make([]packet.NetworkInterface, 0, len(c.NetworkPorts))
-				pmac := c.PrimaryDataMAC()
-				var pip packet.IP
-				for _, ip := range c.IPs {
-					if ip.Family == 4 && ip.Management {
-						pip = ip
-						break
-					}
-				}
-				for _, p := range c.NetworkPorts {
-					ni := packet.NetworkInterface{
-						DHCP: packet.DHCP{
-							Arch:      c.Arch,
-							IfaceName: p.Name,
-							MAC:       p.Data.MAC,
-						},
-					}
-					if *ni.DHCP.MAC == pmac {
-						ni.DHCP.IP = pip
-						ni.Netboot.AllowPXE = c.AllowPXE
-						ni.Netboot.AllowWorkflow = true
-					}
-					if p.Name == "ipmi0" {
-						ni.DHCP.IP = c.IPMI
-						family := 4
-						if ni.DHCP.IP.Address.To16() != nil {
-							family = 6
+		d := packet.HardwareTinkerbellV1{
+			ID: c.ID,
+			Network: packet.Network{
+				Interfaces: func() []packet.NetworkInterface {
+					ifaces := make([]packet.NetworkInterface, 0, len(c.NetworkPorts))
+					pmac := c.PrimaryDataMAC()
+					var pip packet.IP
+					for _, ip := range c.IPs {
+						if ip.Family == 4 && ip.Management {
+							pip = ip
+							break
 						}
-						ni.DHCP.IP.Family = family
 					}
-					ifaces = append(ifaces, ni)
-				}
-				return ifaces
-			}(),
-		},
-		Metadata: packet.Metadata{
-			State:        c.State,
-			BondingMode:  c.BondingMode,
-			Manufacturer: c.Manufacturer,
-			Facility: packet.Facility{
-				PlanSlug:        c.PlanSlug,
-				PlanVersionSlug: c.PlanVersionSlug,
-				FacilityCode:    c.FacilityCode,
+					for _, p := range c.NetworkPorts {
+						ni := packet.NetworkInterface{
+							DHCP: packet.DHCP{
+								Arch:      c.Arch,
+								IfaceName: p.Name,
+								MAC:       p.Data.MAC,
+							},
+						}
+						if *ni.DHCP.MAC == pmac {
+							ni.DHCP.IP = pip
+							ni.Netboot.AllowPXE = c.AllowPXE
+							ni.Netboot.AllowWorkflow = true
+						}
+						if p.Name == "ipmi0" {
+							ni.DHCP.IP = c.IPMI
+							family := 4
+							if ni.DHCP.IP.Address.To16() != nil {
+								family = 6
+							}
+							ni.DHCP.IP.Family = family
+						}
+						ifaces = append(ifaces, ni)
+					}
+					return ifaces
+				}(),
 			},
-		},
-	}
-	d.Metadata.Custom.PreinstalledOS = c.PreinstallOS
-	d.Metadata.Custom.PrivateSubnets = c.PrivateSubnets
+			Metadata: packet.Metadata{
+				State:        c.State,
+				BondingMode:  c.BondingMode,
+				Manufacturer: c.Manufacturer,
+				Facility: packet.Facility{
+					PlanSlug:        c.PlanSlug,
+					PlanVersionSlug: c.PlanVersionSlug,
+					FacilityCode:    c.FacilityCode,
+				},
+			},
+		}
+		d.Metadata.Custom.PreinstalledOS = c.PreinstallOS
+		d.Metadata.Custom.PrivateSubnets = c.PrivateSubnets
 
-	// populate metadata.instance straight from cacher version as boot's Instance struct doesn't have all the attributes we care about
-	b, err := json.Marshal(d)
-	if err != nil {
-		panic(err)
-	}
+		// populate metadata.instance straight from cacher version as boot's Instance struct doesn't have all the attributes we care about
+		b, err := json.Marshal(d)
+		if err != nil {
+			panic(err)
+		}
 
-	m = nil // "clear" out m so that json.Unmarshal doesn't mix in cacher and tinkerbell data layout in the same `m`
-	err = json.Unmarshal(b, &m)
-	if err != nil {
-		panic(err)
-	}
-	if m["metadata"] == nil {
-		m["metadata"] = map[string]interface{}{}
-	}
-	metadata := m["metadata"].(map[string]interface{})
-	metadata["instance"] = instance
-	m["metadata"] = metadata
+		m = nil // "clear" out m so that json.Unmarshal doesn't mix in cacher and tinkerbell data layout in the same `m`
+		err = json.Unmarshal(b, &m)
+		if err != nil {
+			panic(err)
+		}
+		if m["metadata"] == nil {
+			m["metadata"] = map[string]interface{}{}
+		}
+		metadata := m["metadata"].(map[string]interface{})
+		metadata["instance"] = instance
+		m["metadata"] = metadata
 
-	err = json.NewEncoder(os.Stdout).Encode(m)
-	if err != nil {
-		panic(err)
+		err = json.NewEncoder(os.Stdout).Encode(m)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	fmt.Println()
