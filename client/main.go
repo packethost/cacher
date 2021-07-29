2e99650dc6f69ee499336e7bbc65216c2cdffd14
Amy Tobey <atobey@equinix.com>
2021-07-29 13:56:12 -0700

    refactor & add docs
    Moved the anonymous function to a regular private function.
    
    Put godoc on New() and listed which envvars it uses for configuration.
    
    Signed-off-by: Amy Tobey <atobey@equinix.com>

--- client/main.go
+++ client/main.go
@@ -15,19 +15,10 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
+// New returns a new cacher client for the requested facility. It uses the
+// following environment variables for configuration:
+// CACHER_USE_TLS, CACHER_CERT_URL, and CACHER_GRPC_AUTHORITY
 func New(facility string) (cacher.CacherClient, error) {
-	lookupAuthority := func(service, facility string) (string, error) {
-		_, addrs, err := net.LookupSRV(service, "tcp", "cacher."+facility+".packet.net")
-		if err != nil {
-			return "", errors.Wrap(err, "lookup srv record")
-		}
-
-		if len(addrs) < 1 {
-			return "", errors.Errorf("empty responses from _%s._tcp SRV look up", service)
-		}
-
-		return fmt.Sprintf("%s:%d", strings.TrimSuffix(addrs[0].Target, "."), addrs[0].Port), nil
-	}
 	var securityOption grpc.DialOption
 
 	useTLS := env.Bool("CACHER_USE_TLS", true)
@@ -76,4 +67,19 @@ func New(facility string) (cacher.CacherClient, error) {
 		return nil, errors.Wrap(err, "connect to cacher")
 	}
 	return cacher.NewCacherClient(conn), nil
+}
+
+// lookupAuthority does a DNS SRV record lookup for the service in a facility's
+// domain and returns the first address/port combo returned.
+func lookupAuthority(service, facility string) (string, error) {
+	_, addrs, err := net.LookupSRV(service, "tcp", "cacher."+facility+".packet.net")
+	if err != nil {
+		return "", errors.Wrap(err, "lookup srv record")
+	}
+
+	if len(addrs) < 1 {
+		return "", errors.Errorf("empty responses from _%s._tcp SRV look up", service)
+	}
+
+	return fmt.Sprintf("%s:%d", strings.TrimSuffix(addrs[0].Target, "."), addrs[0].Port), nil
 }
