8af3a1c59fba88b5df4bb82f6a5fdbe9ea707823
Thomas Stromberg <t+github@stromberg.org>
2021-08-17 12:15:51 -0700

    Revert "Add lint to tests target, add hadolint (warn-only) & shellcheck"
    This reverts commit d36a4036dc97b2b22fdec305655f87a68755c790.
    
    Signed-off-by: Thomas Stromberg <t+github@stromberg.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -o errexit -o nounset -o pipefail
 
-FACILITY="${FACILITY:-FACILITY_VARIABLE_UNSET}"
-facility=$(echo "$FACILITY" | tr '[:upper:]' '[:lower:]')
-
 if [ -z "${CACHER_TLS_CERT:-}" ]; then
 	(
-		mkdir -p "/certs/${facility}"
-		cd "/certs/${facility}"
-		FACILITY=${facility} sh /tls/gencerts.sh
+		FACILITY=$(echo "$FACILITY" | tr '[:upper:]' '[:lower:]')
+		mkdir -p "/certs/$FACILITY"
+		cd "/certs/$FACILITY"
+		FACILITY=$FACILITY sh /tls/gencerts.sh
 	)
 fi
 
-if [ "$(basename "$1")" == "cacher" ] && [ -z "${CACHER_TLS_CERT:-}" ] && [ -z "${GRPC_CERT:-}" ] && [ -z "${GRPC_KEY:-}" ]; then
-	GRPC_KEY=$(cat "/certs/${facility}/server-key.pem")
-	GRPC_CERT=$(cat "/certs/${facility}/bundle.pem")
+if [ "$(basename $1)" == cacher ] && [ -z "${CACHER_TLS_CERT:-}" ] && [ -z "${GRPC_CERT:-}" ] && [ -z "${GRPC_KEY:-}" ]; then
+	GRPC_KEY=$(cat "/certs/$FACILITY/server-key.pem")
+	GRPC_CERT=$(cat "/certs/$FACILITY/bundle.pem")
 	CACHER_TLS_CERT=$GRPC_CERT
 	export CACHER_TLS_CERT GRPC_CERT GRPC_KEY
 fi
