cd16daa8316336e8bb2717fea2fd36b75c5e0176
Thomas Stromberg <t+github@stromberg.org>
2021-08-17 12:06:01 -0700

    Add lint to tests target, add hadolint (warn-only) & shellcheck
    Signed-off-by: Thomas Stromberg <t+github@stromberg.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 set -o errexit -o nounset -o pipefail
 
+FACILITY="${FACILITY:-FACILITY_VARIABLE_UNSET}"
+facility=$(echo "$FACILITY" | tr '[:upper:]' '[:lower:]')
+
 if [ -z "${CACHER_TLS_CERT:-}" ]; then
 	(
-		FACILITY=$(echo "$FACILITY" | tr '[:upper:]' '[:lower:]')
-		mkdir -p "/certs/$FACILITY"
-		cd "/certs/$FACILITY"
-		FACILITY=$FACILITY sh /tls/gencerts.sh
+		mkdir -p "/certs/${facility}"
+		cd "/certs/${facility}"
+		FACILITY=${facility} sh /tls/gencerts.sh
 	)
 fi
 
-if [ "$(basename $1)" == cacher ] && [ -z "${CACHER_TLS_CERT:-}" ] && [ -z "${GRPC_CERT:-}" ] && [ -z "${GRPC_KEY:-}" ]; then
-	GRPC_KEY=$(cat "/certs/$FACILITY/server-key.pem")
-	GRPC_CERT=$(cat "/certs/$FACILITY/bundle.pem")
+if [ "$(basename "$1")" == "cacher" ] && [ -z "${CACHER_TLS_CERT:-}" ] && [ -z "${GRPC_CERT:-}" ] && [ -z "${GRPC_KEY:-}" ]; then
+	GRPC_KEY=$(cat "/certs/${facility}/server-key.pem")
+	GRPC_CERT=$(cat "/certs/${facility}/bundle.pem")
 	CACHER_TLS_CERT=$GRPC_CERT
 	export CACHER_TLS_CERT GRPC_CERT GRPC_KEY
 fi
